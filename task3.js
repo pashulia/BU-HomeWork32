e56f007218bca1cbb5449ff81cfcd4b15c0b32cf
Pavel Khegay <pashulia.pk@gmail.com>
2023-02-13 21:33:52 +0600

    add added task 3 solution

---
+++ task3.js
@@ -0,0 +1,17 @@
+const Web3 = require('web3');
+
+const web3 = new Web3('https://eth-goerli.g.alchemy.com/v2/XdE1v9zVDSoRe6S5013cteykw1ZDC0u9');
+
+const readlineSync = require('readline-sync');
+
+async function main(){   
+    await web3.eth.getBlock('latest', function(err, block){
+        if (block.gasUsed > 15_000_000) {
+            console.log("currently in use: " + block.gasUsed + " price increases");
+        } else {
+            console.log("currently in use: " + block.gasUsed + " price decreases");
+        }
+    })
+}
+
+main();
